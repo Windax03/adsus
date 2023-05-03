eda58ff787d3ab1912c0f3480fd96c9bbc742e27
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-03 18:56:31 +0200

    Add files via upload

---
+++ practica_5/practica_5/practica5_parte2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#845647, Leris Lacort, Jorge, M, 3, A
+#844759, Villanueva Agudo, Ángel, M, 3, A
+
+if [ $# -ne 1]
+then
+    echo "El script necesita una Ip como parametro"
+    exit 1
+fi
+
+ssh -n as@$IP
+if [ $? -eq 0 ]
+then
+
+    ssh -n as@$IP "sfdisk –s"
+    ssh -n as@$IP "sfdisk –l"
+    ssh -n as@$IP "df -hT"
+
+else
+    echo "$IP no es accesible"
+    exit 1
+fi
