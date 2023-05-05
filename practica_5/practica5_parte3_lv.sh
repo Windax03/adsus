b2cbd07493f03f1eca2f3c17002a9bb5ba7499f0
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-05 12:24:33 +0200

    Add files via upload

--- practica_5/practica5_parte3_lv.sh
+++ practica_5/practica5_parte3_lv.sh
@@ -2,31 +2,57 @@
 #845647, Leris Lacort, Jorge, M, 3, A
 #844759, Villanueva Agudo, Ángel, M, 3, A
 
-OLDIFS=$IFS
+# Comprobacion de usuario y parametros
+if [ "$UID" -ne 0 ]
+then
+
+    echo "Este script necesita privilegios de administrador al usar comandos como lvdisplay"
+    exit 1
+elif [ $# -ne 1 ]
+then
+
+    echo "Este script necesita un fichero como parametro"
+    exit 1
+fi
+
+OldIFS=$IFS
 IFS=','
 while read nombreGrupoVolumen nombreVolumenLogico tamagno tipoSistemaFicheros directorioMontaje
 do
-    vdir=$(lvdisplay "$nombreGrupoVolumen/$nombreVolumenLogico" -Co "lv_path" | grep "$nombreGrupoVolumen/$nombreVolumenLogico" | tr -d '[[:space:]]') &> /dev/null
-    if [ -n "$vdir" ]
+    # Asignamos a vldir el directorio del volumen
+    vldir=$(echo "/dev/$nombreGrupoVolumen/$nombreVolumenLogico")
+    # Miramos si hay coincidencia
+    lvdisplay "$nombreGrupoVolumen/$nombreVolumenLogico" | grep ""$vldir"" &> /dev/null
+    # Si el comando anterior se ejecuta con exite el volumen existe en el sistema
+    if [ $? -eq 0 ]
     then
-        echo "El volumen introducido ya existe,ahora se ampliara"
-        lvextend -L$tamagno $vdir
-        resize2fs $vdir
+        
+        echo "El volumen logico existe en el sistema, a continuacion se procede a ampliarlo"
+        # Reducimos el volumen logico
+        lvextend -L$tamagno $vldir
+        # Agrandar el sistema de ficheros
+        resize2fs $vldir
     else
         echo "El volumen introducido no existe,ahora se creara"
+        # Crear un volumen logico
         lvcreate -L $tamagno -n $nombreVolumenLogico $nombreGrupoVolumen
+        # Comprobamos que el volumen logico se ha podido crear sin problemas
         if [ $? -eq 0 ]
         then
+            # Si no existe el directorio donde montaremos el volumen logico lo creamos
             if [ ! -d $directorioMontaje ]
             then
-                mkdir -p $directorioMontaje
+                # Se crea el directorio de montaje
+                mkdir $directorioMontaje
             fi
-            vdir=$(lvdisplay "$nombreGrupoVolumen/$nombreVolumenLogico" -Co "lv_path" | grep "$nombreGrupoVolumen/$nombreVolumenLogico" | tr -d '[[:space:]]')
-            cat /etc/mtab | grep "/dev/"$directorioMontaje""  >> /etc/fstab
-            mkfs.$tipoSistemaFicheros $vdir
-            mount $vdir $directorioMontaje
+
+            echo -e "$vldir\t$directorioMontaje\t$tipoSistemaFicheros\tdefaults 0 0" >> /etc/fstab
+            # Creamos el sistema de archivos del volumen logico
+            mkfs -t $tipoSistemaFicheros $vldir
+            # Se monta el volumen logico con las especificaciones del tipo, ruta y donde se monta
+            mount -t $tipoSistemaFicheros $vldir $directorioMontaje
         fi
     fi
-    break
-done
-IFS=$OLDIFS
+
+done < $1
+IFS=$OldIFS
