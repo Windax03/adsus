0d2c6e9b60b3d8577ad55a71e20f2d91bd3dbf74
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-19 15:54:33 +0200

    Add files via upload

--- trabajo/firewall2.sh
+++ trabajo/firewall2.sh
@@ -36,9 +36,9 @@ iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
 
 # Permite las conexiones a debian2 (servidor web) y a debian5 (servidor ssh) desde la red Host-Only y desde la red interna 2
 iptables -A FORWARD -i enp0s8 -p tcp --dport 80 -d 192.168.30.2 -j ACCEPT
-iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.31.2 -j ACCEPT
-iptables -A FORWARD -i enp0s9 -p tcp --dport 22 -d 192.168.31.2 -j ACCEPT
-iptables -A FORWARD -i enp0s10 -p tcp --dport 22 -d 192.168.31.2 -j ACCEPT
+iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
+iptables -A FORWARD -i enp0s9 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
+iptables -A FORWARD -i enp0s10 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 
 # Permite que debian1 responda a los pings generados en la intranet, pero no a los generados desde la máquina Host
 iptables -A INPUT -i enp0s9 -p icmp --icmp-type echo-request -j ACCEPT
