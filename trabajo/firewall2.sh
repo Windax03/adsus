f39e8169b197d7be725cf191711bcde331cbf55b
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-19 22:42:18 +0200

    Update firewall2.sh

--- trabajo/firewall2.sh
+++ trabajo/firewall2.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if [ "$UID" -ne 0 ]
 then
     echo "El script necesita privilegios de admin"
@@ -20,24 +18,15 @@ iptables -P FORWARD DROP
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-# Permite el acceso a ssh y http (para el servidor web)
+# Permite el acceso a ssh
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
 # Permite las respuestas de conexiones existentes (incluyendo pings) a ser reenviadas
 iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-# Permitir ping desde las máquinas debianX a Host
-iptables -A FORWARD -i enp0s9 -o enp0s8 -p icmp --icmp-type echo-request -j ACCEPT
-iptables -A FORWARD -i enp0s10 -o enp0s8 -p icmp --icmp-type echo-request -j ACCEPT
-
-# Redirección de peticiones desde el NAT al servidor web de Apache de debian2 y al servidor ssh de debian5
-iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 22 -j DNAT --to 192.168.32.2:22
-iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.30.2:80
-
-# Redirección de peticiones desde el host al servidor web de Apache de debian2 y al servidor ssh de debian5
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.32.2:22
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to 192.168.30.2:80
+# Permite el tráfico interno entre las subredes internas
+iptables -A FORWARD -i enp0s9 -o enp0s10 -j ACCEPT
+iptables -A FORWARD -i enp0s10 -o enp0s9 -j ACCEPT
 
 # Permite el tráfico de las subredes internas a Internet y al Host
 iptables -A FORWARD -i enp0s9 -o enp0s8 -j ACCEPT
@@ -52,10 +41,9 @@ iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 iptables -A FORWARD -i enp0s9 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 
-# Permite el ping entre las máquinas Debian, pero no desde el Host
+# Permite que debian1 responda a los pings generados en la intranet, pero no a los generados desde la máquina Host
 iptables -A INPUT -i enp0s9 -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -i enp0s10 -p icmp --icmp-type echo-request -j ACCEPT
-iptables -A INPUT -i enp0s8 -p icmp --icmp-type echo-request -j DROP
 
 # Preservación de las reglas iptables
 iptables-save > /etc/iptables/rules.v4
