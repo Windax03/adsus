c9f87859fa4ca7abb9176774c8d9b1b21145622f
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-19 22:31:56 +0200

    Update firewall2.sh

--- trabajo/firewall2.sh
+++ trabajo/firewall2.sh
@@ -19,17 +19,29 @@ iptables -P FORWARD DROP
 # Reglas para permitir tráfico local y tráfico relacionado con conexiones existentes
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Permite el acceso a ssh y http (para el servidor web)
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
+# Permite las respuestas de conexiones existentes (incluyendo pings) a ser reenviadas
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Permitir ping desde las máquinas debianX a Host
+iptables -A FORWARD -i enp0s9 -o enp0s8 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A FORWARD -i enp0s10 -o enp0s8 -p icmp --icmp-type echo-request -j ACCEPT
+
+# Redirección de peticiones desde el NAT al servidor web de Apache de debian2 y al servidor ssh de debian5
+iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 22 -j DNAT --to 192.168.32.2:22
+iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.30.2:80
+
+# Redirección de peticiones desde el host al servidor web de Apache de debian2 y al servidor ssh de debian5
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.32.2:22
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to 192.168.30.2:80
+
 # Permite el tráfico de las subredes internas a Internet y al Host
 iptables -A FORWARD -i enp0s9 -o enp0s8 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -o enp0s8 -j ACCEPT
-iptables -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT
-iptables -A FORWARD -i enp0s10 -o enp0s3 -j ACCEPT
 
 # Habilita el NAT para que todas las máquinas debianX puedan acceder a Internet y a la red Host-Only usando la IP pública de debian1
 iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
