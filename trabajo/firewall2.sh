ee416506b96a47bcf537b069736992ae97458302
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-19 17:03:59 +0200

    Add files via upload

--- trabajo/firewall2.sh
+++ trabajo/firewall2.sh
@@ -30,22 +30,16 @@ iptables -A FORWARD -i enp0s10 -o enp0s9 -j ACCEPT
 # Permite el tráfico de las subredes internas a Internet y al Host
 iptables -A FORWARD -i enp0s9 -o enp0s8 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -o enp0s8 -j ACCEPT
+iptables -A FORWARD -i enp0s8 -o enp0s8 -d 192.168.32.0/24 -j ACCEPT  # para Debian5
 
 # Habilita el NAT para que todas las máquinas debianX puedan acceder a Internet y a la red Host-Only usando la IP pública de debian1
 iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
 
 # Permite las conexiones a debian2 (servidor web) y a debian5 (servidor ssh) desde la red Host-Only y desde la red interna 2
 iptables -A FORWARD -i enp0s8 -p tcp --dport 80 -d 192.168.30.2 -j ACCEPT
-iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
+iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT  # desde la red host-only
 iptables -A FORWARD -i enp0s9 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
-
-# Se permite el paso del tráfico hacia la extranet (SSH y servidor web)
-iptables -A FORWARD -i enp0s3 -p all -j ACCEPT
-iptables -A FORWARD -i enp0s8 -p all -j ACCEPT
-
-# Redirección de las peticiones SSH desde la red Host-Only al servidor ssh de debian5
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to-destination 192.168.32.2:22
 
 # Permite que debian1 responda a los pings generados en la intranet, pero no a los generados desde la máquina Host
 iptables -A INPUT -i enp0s9 -p icmp --icmp-type echo-request -j ACCEPT
