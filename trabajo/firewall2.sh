44158b44438131e336c2ed02945ae6a43f487afd
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-19 22:37:35 +0200

    Update firewall2.sh

--- trabajo/firewall2.sh
+++ trabajo/firewall2.sh
@@ -44,7 +44,7 @@ iptables -A FORWARD -i enp0s9 -o enp0s8 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -o enp0s8 -j ACCEPT
 
 # Habilita el NAT para que todas las máquinas debianX puedan acceder a Internet y a la red Host-Only usando la IP pública de debian1
-iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
 
 # Permite las conexiones a debian2 (servidor web) y a debian5 (servidor ssh) desde la red Host-Only y desde la red interna 2
 iptables -A FORWARD -i enp0s8 -p tcp --dport 80 -d 192.168.30.2 -j ACCEPT
