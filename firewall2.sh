3143fd2c5632ede3a6f50994e70bfc694bd6eaf8
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-20 08:38:26 +0200

    Add files via upload

--- firewall2.sh
+++ firewall2.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$UID" -ne 0 ]
 then
     echo "El script necesita privilegios de admin"
@@ -36,9 +38,9 @@ iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i enp0s9 -o enp0s10 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -o enp0s9 -j ACCEPT
 
-# Permite el tráfico de las subredes internas a Internet y al Host
-iptables -A FORWARD -i enp0s9 -o enp0s8 -j ACCEPT
-iptables -A FORWARD -i enp0s10 -o enp0s8 -j ACCEPT
+# Permite el tráfico de las subredes internas a Internet
+iptables -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT
+iptables -A FORWARD -i enp0s10 -o enp0s3 -j ACCEPT
 
 # Habilita el NAT para que todas las máquinas debianX puedan acceder a Internet y a la red Host-Only usando la IP pública de debian1
 iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
