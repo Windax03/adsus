3ebc43c1f68b7747ad397f0d20652e37e72a7f25
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-20 09:09:31 +0200

    Add files via upload

--- trabajo/firewall2.sh
+++ trabajo/firewall2.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if [ "$UID" -ne 0 ]
 then
     echo "El script necesita privilegios de admin"
@@ -14,7 +12,7 @@ iptables -t mangle -F
 # Politicas por defecto
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
+iptables -P FORWARD DROP
 
 # Reglas para permitir tráfico local y tráfico relacionado con conexiones existentes
 iptables -A INPUT -i lo -j ACCEPT
@@ -30,12 +28,15 @@ iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i enp0s9 -o enp0s10 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -o enp0s9 -j ACCEPT
 
-# Permite el tráfico de las subredes internas a Internet
-iptables -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT
-iptables -A FORWARD -i enp0s10 -o enp0s3 -j ACCEPT
+# Permite el tráfico de las subredes internas a Internet y al Host
+iptables -A FORWARD -i enp0s9 -o enp0s8 -j ACCEPT
+iptables -A FORWARD -i enp0s10 -o enp0s8 -j ACCEPT
 
 # Habilita el NAT para que todas las máquinas debianX puedan acceder a Internet y a la red Host-Only usando la IP pública de debian1
 iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o enp0s9 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o enp0s10 -j MASQUERADE
 
 # Permite las conexiones a debian2 (servidor web) y a debian5 (servidor ssh) desde la red Host-Only y desde la red interna 2
 iptables -A FORWARD -i enp0s8 -p tcp --dport 80 -d 192.168.30.2 -j ACCEPT
@@ -43,16 +44,9 @@ iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 iptables -A FORWARD -i enp0s9 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 
-# Permite el acceso a internet desde debian1 (necesario para el servidor web)
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-
 # Permite que debian1 responda a los pings generados en la intranet, pero no a los generados desde la máquina Host
 iptables -A INPUT -i enp0s9 -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -i enp0s10 -p icmp --icmp-type echo-request -j ACCEPT
-
-# Bloquea los pings desde la máquina Host
-iptables -A INPUT -i enp0s8 -p icmp --icmp-type echo-request -j DROP
 
 # Preservación de las reglas iptables
 iptables-save > /etc/iptables/rules.v4
