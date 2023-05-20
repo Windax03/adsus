1e23ba22c50ff32701c60ed21935bf9d9022d5f9
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-20 08:51:14 +0200

    Add files via upload

--- firewall2.sh
+++ firewall2.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-# Comprobar si el script se ejecuta como root
-if [ "$UID" -ne 0 ]; then
-    echo "El script necesita privilegios de administrador."
+if [ "$UID" -ne 0 ]
+then
+    echo "El script necesita privilegios de admin"
     exit 1
 fi
 
-# Limpiar todas las reglas existentes
+# Limpiamos reglas existentes
 iptables -F
 iptables -t nat -F
 iptables -t mangle -F
 
-# Establecer políticas predeterminadas
+# Politicas por defecto
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
-iptables -P FORWARD DROP
+iptables -P FORWARD ACCEPT
 
-# Permitir tráfico local y tráfico relacionado con conexiones existentes
+# Reglas para permitir tráfico local y tráfico relacionado con conexiones existentes
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-# Permitir el acceso a ssh
+# Permite el acceso a ssh
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# Permitir a las máquinas debian acceso a Internet
+# Permite las respuestas de conexiones existentes (incluyendo pings) a ser reenviadas
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Permite el tráfico interno entre las subredes internas
+iptables -A FORWARD -i enp0s9 -o enp0s10 -j ACCEPT
+iptables -A FORWARD -i enp0s10 -o enp0s9 -j ACCEPT
+
+# Permite el tráfico de las subredes internas a Internet
 iptables -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT
 iptables -A FORWARD -i enp0s10 -o enp0s3 -j ACCEPT
 
-# Permitir respuestas a conexiones ya establecidas
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# Habilitar NAT para que las máquinas debian puedan usar la IP pública de debian1 para acceder a Internet
+# Habilita el NAT para que todas las máquinas debianX puedan acceder a Internet y a la red Host-Only usando la IP pública de debian1
 iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+
+# Permite las conexiones a debian2 (servidor web) y a debian5 (servidor ssh) desde la red Host-Only y desde la red interna 2
+iptables -A FORWARD -i enp0s8 -p tcp --dport 80 -d 192.168.30.2 -j ACCEPT
+iptables -A FORWARD -i enp0s8 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
+iptables -A FORWARD -i enp0s9 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
+iptables -A FORWARD -i enp0s10 -p tcp --dport 22 -d 192.168.32.2 -j ACCEPT
 
 # Permite que debian1 responda a los pings generados en la intranet, pero no a los generados desde la máquina Host
 iptables -A INPUT -i enp0s9 -p icmp --icmp-type echo-request -j ACCEPT
